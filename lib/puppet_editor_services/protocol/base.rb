923067964d9ec0487d22bcf04416bd172ee7803b
Glenn Sarti <glennsarti@users.noreply.github.com>
2019-10-02 17:22:15 +0800

    (GH-193) Refactor Editor Services and Language Server
    Previously the both the Editor Services and Language Server have grown
    organically over time. It is now complicated to understand how the Server,
    Client connection and message routing work as their scopes have been bleeding
    into other domains.  For example the message_router also can send JSON RPC
    messages.
    
    This commit changes the architecture of the Editor Services and the Language
    Server to use much cleaner architecture.
    
    Note that the refactor should only touch from the Message Router down to the
    Server level (e.g. TCP server). The Validation Queue is modified as it calls
    back into the server due to async. processing.
    
    The new architecture moves the ALL of the following into the Puppet Editor
    Services namespace:
    
    Server <--> Connection <--> Protocol <--> Handler
    
    - The Server is responsible for sending and receiving "bytes-on-the-wire" (TCP,
      STDIO)
    
    - The Connection is created per client and manages the connection to the client
      (e.g. close/error)  The connection sends and receives arrays of bytes to/from
      the Protocol object
    
    - The Protocol object is responsible for receiving bytes from the Connection and
      coverting them into messages, and vice-versa (turning messages into byte
      arrays to the connection)
    
    - The Handler reads and writes messages to/from the protocol.
    
    There will normally be one object-chain per connection e.g. For three connections;
    
            +- Connection -- Protocol -- Handler
            |
    Server -+- Connection -- Protocol -- Handler
            |
            +- Connection -- Protocol -- Handler
    
    For the purposes of the Language and Debug Server, Only the JSON RPC protocol
    and base JSON RPC handler are implemented.
    
    The Language Server then implements it's own Handler in the Puppet Language Server namespace:
    
    Language Server Handler <--> Puppet Providers/Helpers etc.
    
    * This handler takes messages, processes them and optionally sends a reply.
    
    * Note that the complex server-to-client request/response process is handled by
      the JSON RPC protocol layer.

---
+++ lib/puppet_editor_services/protocol/base.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module PuppetEditorServices
+  module Protocol
+    class Base
+      attr_reader :connection
+      attr_reader :handler
+
+      def initialize(connection)
+        @connection = connection
+        @handler = connection.server.handler_options[:class].new(self)
+      end
+
+      # @abstract
+      def receive_data(data); end
+    end
+  end
+end
