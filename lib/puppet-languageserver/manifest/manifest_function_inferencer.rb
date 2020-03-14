6f679675a22a5ac7f54f7c33072514e89cded98a
Glenn Sarti <glennsarti@users.noreply.github.com>
2020-03-14 20:15:53 +0800

    inferencer

---
+++ lib/puppet-languageserver/manifest/manifest_function_inferencer.rb
@@ -0,0 +1,292 @@
+# frozen_string_literal: true
+
+#require 'puppet/pops/types/type_factory'
+#require 'puppet/pops/types/type_parser'
+
+module PuppetLanguageServer
+  module Manifest
+    module ManifestInferencer
+      class ManifestFunctionInferencer
+        def initialize(type_detector, object_cache = nil)
+          @type_detector = type_detector
+          @object_cache = object_cache
+          @type_parser = Puppet::Pops::Types::TypeParser.new
+        end
+
+        def calculate_function_call_types(func_name, parameter_types, func_lambda)
+          result = calculate_default_function_call_types(func_name, parameter_types, func_lambda)
+
+          if result.nil? && !@object_cache.nil?
+            result = calculate_object_cache_function_call(func_name, parameter_types, func_lambda)
+          end
+
+          # puts result
+          # TODO return if no lambda
+
+          # Setup lambda parameter type definitions
+          # make sure we call infer(lambda) to do child inferences
+          result
+        end
+
+        def calculate_object_cache_function_call(func_name, parameter_types, func_lambda)
+          func = @object_cache.object_by_name(:function, func_name, fuzzy_match: true)
+          return nil if func.nil? || func.signatures.empty?
+
+          # We have a function, now find an appropriate signature
+          # TODO: Also match type? For now just match the number of params
+          func_sig = func.signatures.find { |sig| sig.parameters.count == parameter_types.count }
+          return nil if func_sig.nil?
+          return @type_detector.default_type if func_sig.return_types.nil? || func_sig.return_types.empty?
+
+          type_text = func_sig.return_types.count == 1 ? func_sig.return_types[0] : 'Variant[' + func_sig.return_types.join(',') + ']'
+          # I don't like this ... but ... best we've got.
+          @type_parser.parse(type_text, nil)
+        end
+
+        def calculate_default_function_call_types(func_name, parameter_types, func_lambda)
+          # https://puppet.com/docs/puppet/latest/function.html
+          # C:\Source\puppet\lib\puppet\functions
+          case func_name
+          when 'abs'
+            calculate_abs_function(parameter_types, func_lambda)
+          when 'alert'
+            Puppet::Pops::Types::TypeFactory.undef
+          # all
+          # annotate
+          # any
+          # assert_type
+          when 'binary_file'
+            Puppet::Pops::Types::TypeFactory.binary
+          when 'break'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'call'
+            @type_detector.default_type
+          # camelcase
+          # capitalize
+          when 'ceiling'
+            Puppet::Pops::Types::TypeFactory.integer
+          # chomp
+          # chop
+          when 'compare'
+            Puppet::Pops::Types::TypeFactory.range(-1, 1)
+          # contain
+          # convert_to
+          # create_resources
+          when 'crit'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'debug'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'defined'
+            Puppet::Pops::Types::TypeFactory.boolean
+          when 'dig'
+            @type_detector.default_type
+          when 'digest'
+            Puppet::Pops::Types::TypeFactory.string
+          # downcase
+          # each
+          when 'emerg'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'empty'
+            Puppet::Pops::Types::TypeFactory.boolean
+          when 'epp'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'err'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'eyaml_lookup_key'
+            @type_detector.default_type
+          when 'fail'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'file'
+            Puppet::Pops::Types::TypeFactory.string
+          # filter
+          when 'find_file'
+            Puppet::Pops::Types::TypeFactory.variant(Puppet::Pops::Types::TypeFactory.string, Puppet::Pops::Types::TypeFactory.undef)
+          # flatten
+          when 'floor'
+            Puppet::Pops::Types::TypeFactory.integer
+          when 'fqdn_rand'
+            Puppet::Pops::Types::TypeFactory.integer
+          when 'generate'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'get'
+            @type_detector.default_type
+          when 'getvar'
+            @type_detector.default_type
+          # group_by
+          when 'hiera'
+            @type_detector.default_type
+          when 'hiera_array'
+            Puppet::Pops::Types::TypeFactory.array_of_any
+          when 'hiera_hash'
+            Puppet::Pops::Types::TypeFactory.hash_of_any
+          # hiera_include
+          # hocon_data
+          # import
+          when 'include'
+            # TODO: Should actually be Array[Type[Class]]
+            Puppet::Pops::Types::TypeFactory.array_of_any
+          when 'index'
+            Puppet::Pops::Types::TypeFactory.integer
+          when 'info'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'inline_epp'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'inline_template'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'join'
+            Puppet::Pops::Types::TypeFactory.string
+          # json_data
+          # keys
+          when 'length'
+            Puppet::Pops::Types::TypeFactory.range(0, 'default')
+          when 'lest'
+            @type_detector.default_type
+          when 'lookup'
+            @type_detector.default_type
+          # lstrip
+          when 'map'
+            calculate_map_function(parameter_types, func_lambda)
+          # match
+          when 'max'
+            @type_detector.default_type
+          when 'md5'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'min'
+            @type_detector.default_type
+          when 'module_directory'
+            Puppet::Pops::Types::TypeFactory.variant(Puppet::Pops::Types::TypeFactory.string, Puppet::Pops::Types::TypeFactory.undef)
+
+          # new
+          # next
+          when 'notice'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'partition'
+            Puppet::Pops::Types::TypeFactory.tuple(
+              [
+                Puppet::Pops::Types::TypeFactory.array_of_any,
+                Puppet::Pops::Types::TypeFactory.array_of_any
+              ]
+            )
+          when 'realize'
+            Puppet::Pops::Types::TypeFactory.undef
+          # reduce
+          # regsubst
+          when 'require'
+            Puppet::Pops::Types::TypeFactory.undef
+          # return
+          # reverse_each
+          when 'round'
+            Puppet::Pops::Types::TypeFactory.integer
+          # rstrip
+          # scanf
+          when 'sha1'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'sha256'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'shellquote'
+            Puppet::Pops::Types::TypeFactory.string
+
+          # size
+          # slice
+          # sort
+          when 'split'
+            Puppet::Pops::Types::TypeFactory.array_of(Puppet::Pops::Types::TypeFactory.string)
+          when 'sprintf'
+            Puppet::Pops::Types::TypeFactory.undef
+          # step
+          when 'strftime'
+            Puppet::Pops::Types::TypeFactory.string
+          # strip
+          when 'tag'
+            Puppet::Pops::Types::TypeFactory.undef
+          when 'tagged'
+            Puppet::Pops::Types::TypeFactory.boolean
+          when 'template'
+            Puppet::Pops::Types::TypeFactory.string
+          when 'then'
+            @type_detector.default_type
+          when 'tree_each'
+            @type_detector.default_type
+          when 'type'
+            # TODO: not strictly true! should just return the inferred type
+            @type_detector.default_type
+          # unique
+          # unwrap
+          # upcase
+          # values
+          when 'versioncmp'
+            Puppet::Pops::Types::TypeFactory.range(-1, 1)
+          when 'warning'
+            Puppet::Pops::Types::TypeFactory.undef
+          # with
+          when 'yaml_data'
+            @type_detector.default_type
+          else
+            nil
+          end
+        end
+
+        def calculate_abs_function(parameter_types, func_lambda)
+          return @type_detector.default_type unless parameter_types.count == 1
+          return Puppet::Pops::Types::TypeFactory.numeric if parameter_types[0].is_a?(Puppet::Pops::Types::PStringType)
+
+          param_type = parameter_types[0]
+          if param_type.is_a?(Puppet::Pops::Types::PNumericType)
+            new_from = param_type.from.nil? ? nil : param_type.from.abs
+            new_to = param_type.to.nil? ? nil : param_type.to.abs
+            return param_type.class.new(new_from, new_to)
+          end
+
+          @type_detector.default_type
+        end
+
+        def calculate_map_function(parameter_types, func_lambda)
+          # https://puppet.com/docs/puppet/latest/function.html#map
+          return_type = Puppet::Pops::Types::TypeFactory.array_of_any
+          # If we have no parameters or there's no block, there's no need to continue
+          return return_type if parameter_types.empty? || func_lambda.nil?
+          lambda_inf = @type_detector.find_inference(func_lambda)
+          return return_type if lambda_inf.nil?
+
+          caller_type = parameter_types[0]
+
+          set_param_types(lambda_inf) do |param_count|
+            if caller_type.is_a?(Puppet::Pops::Types::PHashType)
+              if param_count == 2
+                [caller_type.key_type, caller_type.value_type]
+              elsif param_count == 1
+                tl = @type_detector.squash_type_list([caller_type.key_type, caller_type.value_type])
+                [
+                  Puppet::Pops::Types::TypeFactory.array_of(
+                    Puppet::Pops::Types::TypeFactory.variant(*tl),
+                    Puppet::Pops::Types::TypeFactory.range(2, 2)
+                  )
+                ]
+              end
+            elsif caller_type.is_a?(Puppet::Pops::Types::PArrayType)
+              if param_count == 2
+                [Puppet::Pops::Types::TypeFactory.integer, caller_type.element_type]
+              elsif param_count == 1
+                [caller_type.element_type]
+              end
+            end
+          end
+
+          return_type
+        end
+
+        def set_param_types(lambda_inf, &block)
+          params = lambda_inf.children.select { |child| child.is_a?(ParameterInference) }
+          new_param_types = yield params.count
+          return if new_param_types.nil? || !new_param_types.is_a?(Array) || new_param_types.empty?
+
+          new_param_types.each_with_index do |item, index|
+            next if item.nil?
+            params[index].puppet_type = item
+          end
+          nil
+        end
+      end
+    end
+  end
+end
---
+++ spec/languageserver/unit/puppet-languageserver/manifest/manifest_function_inferencer_spec.rb
@@ -0,0 +1,426 @@
+require 'spec_helper'
+require 'puppet-languageserver/manifest/manifest_inferencer'
+
+# Monkey Patch
+require 'puppet/pal/pal_api'
+require 'puppet/pal/compiler'
+class Puppet::Pal::Compiler
+  def public_top_scope
+    topscope
+  end
+end
+# End Monkey patch
+
+RSpec::Matchers.define :be_an_inference do |name = nil, klass = nil|
+  match do |actual|
+    return false unless actual.is_a?(PuppetLanguageServer::Manifest::ManifestInferencer::BaseInference)
+
+    (name.nil? || actual.name == name) && (klass.nil? || actual.is_a?(klass))
+  end
+
+  failure_message do |actual|
+    message_name = name.nil? ? 'anything' : "'#{name}'"
+    "expected that #{actual.nil? ? 'nil' : actual.to_s} would be an inference called #{message_name}, of type #{klass.nil? ? PuppetLanguageServer::Manifest::ManifestInferencer::BaseInference : klass}"
+  end
+
+  description do
+    "be an inference called '#{name}' of type #{klass}"
+  end
+end
+
+RSpec::Matchers.define :be_inference_type do |puppet_typename|
+  match do |actual|
+    return false unless actual.respond_to?(:puppet_type)
+    return false unless actual.puppet_type.is_a?(Puppet::Pops::Types::PuppetObject)
+
+    actual.puppet_type.to_s == puppet_typename
+  end
+
+  failure_message do |actual|
+    if !actual.respond_to?(:puppet_type)
+      "expected that #{actual} would respond to 'puppet_type'"
+    elsif !actual.is_a?(Puppet::Pops::Types::PuppetObject)
+      "expected that #{actual.class} would be a 'Puppet::Pops::Types::PuppetObject'"
+    else
+      "expected that #{actual.puppet_type.to_s} would be #{puppet_typename}"
+    end
+  end
+
+  description do
+    "be an inference with type #{puppet_typename}"
+  end
+end
+
+RSpec.shared_examples 'a type detector' do |var_name|
+  it "detects variable called #{var_name} the same as a real Puppet compiler" do
+    inferencer, varmap = inference_and_actual_result(content, true)
+    item = inferencer.find(var_name, PuppetLanguageServer::Manifest::ManifestInferencer::VariableInference)
+    expect(item).to be_an_inference
+
+    expect(item.puppet_type).to be_a(Puppet::Pops::Types::PuppetObject)
+    expect(item.puppet_type.name).to eq(varmap[var_name].name)
+  end
+end
+
+RSpec.shared_examples 'an exact type detector' do |var_name|
+  it "detects variable called #{var_name} the same as a real Puppet compiler" do
+    inferencer, varmap = inference_and_actual_result(content, true)
+    item = inferencer.find(var_name, PuppetLanguageServer::Manifest::ManifestInferencer::VariableInference)
+    expect(item).to be_an_inference
+
+    expect(item.puppet_type).to be_a(Puppet::Pops::Types::PuppetObject)
+    expect(item.puppet_type.to_s).to eq(varmap[var_name].to_s)
+  end
+end
+
+RSpec.shared_examples 'a type inferrer' do |var_name, puppet_typename|
+  it "detects variable called #{var_name} of type #{puppet_typename}" do
+    result = inference_result(content)
+    item = result.find(var_name, PuppetLanguageServer::Manifest::ManifestInferencer::VariableInference)
+    expect(item).to be_an_inference
+
+    expect(item.puppet_type).to be_a(Puppet::Pops::Types::PuppetObject)
+    expect(item.puppet_type.name).to eq(puppet_typename)
+  end
+end
+
+RSpec.shared_examples 'an exact type inferrer' do |var_name, puppet_typename|
+  it "detects variable called #{var_name} of type #{puppet_typename}" do
+    inferencer = inference_result(content)
+    item = inferencer.find(var_name, PuppetLanguageServer::Manifest::ManifestInferencer::VariableInference)
+    expect(item).to be_an_inference
+
+    expect(item.puppet_type).to be_a(Puppet::Pops::Types::PuppetObject)
+    expect(item.puppet_type.to_s).to eq(puppet_typename)
+  end
+end
+
+describe 'PuppetLanguageServer::Manifest::ManifestInferencer' do
+  describe 'PuppetLanguageServer::Manifest::ManifestInferencer::ManifestFunctionInferencer' do
+    def inference_result(content, tasks_mode = true)
+      parser = Puppet::Pops::Parser::Parser.new
+      ast = parser.singleton_parse_string(content, tasks_mode, '')
+
+      inferencer = PuppetLanguageServer::Manifest::ManifestInferencer::ManifestInferences.new
+      inferencer.infer(ast)
+      inferencer
+    end
+
+    def inference_and_actual_result(content, tasks_mode = true)
+      parser = Puppet::Pops::Parser::Parser.new
+      ast = parser.singleton_parse_string(content, tasks_mode, '')
+
+      inferencer = PuppetLanguageServer::Manifest::ManifestInferencer::ManifestInferences.new
+      inferencer.infer(ast)
+
+      Puppet.initialize_settings unless Puppet.settings.global_defaults_initialized?
+      topscope = Puppet::Pal.in_tmp_environment('pal_env',
+        modulepath: [],
+        facts: {}
+        ) do |pal|
+          pal.with_script_compiler { |c|
+            c.evaluate(ast)
+            c.public_top_scope
+          }
+        end
+      tc = Puppet::Pops::Types::TypeCalculator.new
+
+      # Munge the topscope
+      varmap = {}
+      topscope.to_hash
+              .reject { |k, _| k == 'trusted' || k == 'facts' || k == 'server_facts' }
+              .each { |k, v| varmap[k] = v.is_a?(Puppet::Pops::Types::PAnyType) ? v : tc.infer(v) }
+
+      [inferencer, varmap]
+    end
+
+    context 'using functions which can not be inferred' do
+      # These functions are just too hard to make sensible decisions on so they always return a known variation on Any
+      # e.g. Any, Array[Any], Hash[Any, Any] etc.
+      let(:content) do
+        <<-EOT
+        $result1 = dig({}, 'a', 'b', 1, 'x')
+        $result2 = {}.dig('a', 'b', 1, 'x')
+        $result3 = eyaml_lookup_key('key', {})
+        $result4 = get($facts, 'os.family')
+        $result5 = $facts.get('os.family')
+        $result6 = $facts.get('os.family') |$p1| { }
+        $result7 = getvar('facts')
+        $result8 = getvar('facts') |$p2| { }
+        $result9 = hiera('users', undef)
+        $result10 = hiera_array('users', undef)
+        $result11 = hiera_hash('users', undef)
+        $result12 = lest($x) || { do_things() }
+        $result13 = [].lest || { }
+        $result14 = lookup('users')
+        $result15 = lookup(['users'])
+        $result16 = lookup('users') |$p3| { }
+        $result17 = max("1", 2)
+        $result18 = [1,2,3].max
+        $result19 = [1,2,3].max |$p4, $p5| { compare($a, $b) }
+        $result20 = min("1", 2)
+        $result21 = [1,2,3].min
+        $result22 = [1,2,3].min |$p6, $p7| { compare($a, $b) }
+        $result23 = ['', b, c].partition |$p8| { }
+        $result24 = partition(['', b, c]) |$p9| { }
+        $result25 = then($x) || { do_things() }
+        $result26 = [].then || { }
+        $result27 = [].tree_each |$p10, $p11| {}
+        $result28 = tree_each([]) |$p12, $p13| {}
+        $result29 = yaml_data('users')
+        EOT
+      end
+
+      it_behaves_like 'an exact type inferrer', 'result1', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result2', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result3', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result4', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result5', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result6', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result7', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result8', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result9', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result10', 'Array'
+      it_behaves_like 'an exact type inferrer', 'result11', 'Hash'
+      it_behaves_like 'an exact type inferrer', 'result12', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result13', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result14', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result15', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result16', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result17', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result18', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result19', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result20', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result21', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result22', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result24', 'Tuple[Array, Array]'
+      it_behaves_like 'an exact type inferrer', 'result25', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result26', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result27', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result28', 'Any'
+      it_behaves_like 'an exact type inferrer', 'result29', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p1', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p2', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p3', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p4', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p5', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p6', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p7', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p8', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p9', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p10', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p11', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p12', 'Any'
+      it_behaves_like 'an exact type inferrer', 'p13', 'Any'
+    end
+
+    context 'using returnless functions' do
+      let(:content) do
+        <<-EOT
+        $result1 = alert("something")
+        $result2 = crit("something")
+        $result3 = debug("something")
+        $result4 = emerg("something")
+        $result5 = err("something")
+        $result6 = fail("something")
+        $result7 = info("something")
+        $result8 = notice("something")
+        $result9 = realize("something")
+        $result10 = require("something")
+        $result11 = sprintf("something")
+        $result12 = tag("something")
+        $result13 = warning("something")
+        $result14 = break()
+
+        EOT
+      end
+
+      # Returnless functions won't really compile
+      it_behaves_like 'a type inferrer', 'result1', 'Undef'
+      it_behaves_like 'a type inferrer', 'result2', 'Undef'
+      it_behaves_like 'a type inferrer', 'result3', 'Undef'
+      it_behaves_like 'a type inferrer', 'result4', 'Undef'
+      it_behaves_like 'a type inferrer', 'result5', 'Undef'
+      it_behaves_like 'a type inferrer', 'result6', 'Undef'
+      it_behaves_like 'a type inferrer', 'result7', 'Undef'
+      it_behaves_like 'a type inferrer', 'result8', 'Undef'
+      it_behaves_like 'a type inferrer', 'result9', 'Undef'
+      it_behaves_like 'a type inferrer', 'result10', 'Undef'
+      it_behaves_like 'a type inferrer', 'result11', 'Undef'
+      it_behaves_like 'a type inferrer', 'result12', 'Undef'
+      it_behaves_like 'a type inferrer', 'result13', 'Undef'
+      it_behaves_like 'a type inferrer', 'result14', 'Undef'
+    end
+
+    context 'using string functions' do
+      let(:content) do
+        <<-EOT
+        $result1 = digest('abc')
+        # $result2 = epp('abc')
+        # $result3 = file('abc')
+        # $result4 = generate('abc')
+        # $result5 = inline_epp('abc')
+        # $result6 = inline_template('abc')
+        $result2 = [1,2,3].join
+        $result3 = join([1,2,3])
+        $result4 = md5('abc')
+        $result5 = sha1('abc')
+        $result6 = sha256('abc')
+        $result7 = shellquote(['abc', '123'])
+        $result8 = strftime('abc')
+        # $result13 = template('abc')
+        EOT
+      end
+
+      it_behaves_like 'a type detector', 'result1'
+      it_behaves_like 'a type detector', 'result2'
+      it_behaves_like 'a type detector', 'result3'
+      it_behaves_like 'a type detector', 'result4'
+      it_behaves_like 'a type detector', 'result5'
+      it_behaves_like 'a type detector', 'result6'
+      it_behaves_like 'a type detector', 'result7'
+      it_behaves_like 'a type detector', 'result8'
+
+      context 'with a manifest that puppet can\'t compile' do
+        let(:content) do
+          <<-EOT
+          $result1 = epp('abc')
+          $result2 = file('abc')
+          $result3 = generate('abc')
+          $result4 = inline_epp('abc')
+          $result5 = inline_template('abc')
+          $result6 = template('abc')
+          EOT
+        end
+
+        it_behaves_like 'a type inferrer', 'result1', 'String'
+        it_behaves_like 'a type inferrer', 'result2', 'String'
+        it_behaves_like 'a type inferrer', 'result3', 'String'
+        it_behaves_like 'a type inferrer', 'result4', 'String'
+        it_behaves_like 'a type inferrer', 'result5', 'String'
+        it_behaves_like 'a type inferrer', 'result6', 'String'
+      end
+
+    end
+
+    context 'using binary functions' do
+      let(:content) do
+        <<-EOT
+        $result1 = binary_file('abc')
+        EOT
+      end
+
+      it_behaves_like 'a type inferrer', 'result1', 'Binary'
+    end
+
+    context 'using integer functions' do
+      let(:content) do
+        <<-EOT
+        $result1 = ceiling(1.2)
+        $result2 = compare(1, 1.2)
+        $result3 = floor(1.2)
+        $result4 = ['a', 'b'].index |$value| { $value == 'a' }
+        $result5 = index(['a', 'b']) |$value| { $value == 'a' }
+        $result6 = round(1.2)
+        $result7 = versioncmp('1.0', '2.0')
+        EOT
+      end
+
+      it_behaves_like 'a type detector', 'result1'
+      it_behaves_like 'a type detector', 'result2'
+      it_behaves_like 'a type detector', 'result3'
+      it_behaves_like 'a type detector', 'result4'
+      it_behaves_like 'a type detector', 'result5'
+      it_behaves_like 'a type detector', 'result6'
+      it_behaves_like 'a type detector', 'result7'
+
+      context 'with a manifest that puppet can\'t compile' do
+        let(:content) do
+          <<-EOT
+          $result1 = fqdn_rand(1)
+          EOT
+        end
+
+        it_behaves_like 'a type inferrer', 'result1', 'Integer'
+      end
+    end
+
+    context 'using the abs function' do
+      let(:content) do
+        <<-EOT
+        $var1 = abs(-1.0)
+        $var2 = abs(1.0)
+        $var3 = abs(-1)
+        $var4 = abs(1)
+        $var5 = abs("-1.0")
+        EOT
+      end
+
+      it_behaves_like 'an exact type detector', 'var1'
+      it_behaves_like 'an exact type detector', 'var2'
+      it_behaves_like 'an exact type detector', 'var3'
+      it_behaves_like 'an exact type detector', 'var4'
+      # Can't resolve strings but they are at least numerics
+      it_behaves_like 'a type inferrer', 'var5', 'Numeric'
+    end
+
+    context 'using the map function' do
+      context 'with arrays' do
+        let(:content) do
+          <<-EOT
+          $var1 = ["a", "b"]
+          $var2 = $var1.map |$p1| { 2 }
+          $var3 = map($var1) |$p2| { 2 }
+          $var4 = $var1.map |$p3, $p4| { 2 }
+          EOT
+        end
+
+        it_behaves_like 'a type detector', 'var1'
+        # puppet compiler won't see $n, but we do
+        it_behaves_like 'a type inferrer', 'p1', 'String'
+        it_behaves_like 'a type inferrer', 'p2', 'String'
+        it_behaves_like 'a type inferrer', 'p3', 'Integer'
+        it_behaves_like 'a type inferrer', 'p4', 'String'
+        # We cant ever _really_ know the the result of .map so assume an Array[Any]
+        it_behaves_like 'a type inferrer', 'var2', 'Array'
+        it_behaves_like 'a type inferrer', 'var3', 'Array'
+      end
+
+      context 'with hashes' do
+        let(:content) do
+          <<-EOT
+          $var1 = { "a" => 2 }
+          $var2 = $var1.map |$p1| { 2 }
+          $var3 = map($var1) |$p2| { 2 }
+          $var4 = $var1.map |$p3, $p4| { 2 }
+          EOT
+        end
+
+        it_behaves_like 'an exact type detector', 'var1'
+        # puppet compiler won't see $n, but we do
+        it_behaves_like 'a type inferrer', 'p1', 'Array'
+        it_behaves_like 'a type inferrer', 'p2', 'Array'
+        it_behaves_like 'a type inferrer', 'p3', 'String'
+        it_behaves_like 'a type inferrer', 'p4', 'Integer'
+        # We cant ever _really_ know the the result of .map so assume an Array[Any]
+        it_behaves_like 'a type inferrer', 'var2', 'Array'
+        it_behaves_like 'a type inferrer', 'var3', 'Array'
+      end
+
+      context 'with an explicit type' do
+        let(:content) do
+          <<-EOT
+          $var1 = [1, 2, 3]
+          $var2 = $var1.map |Numeric $n| { 2 }
+          EOT
+        end
+
+        it_behaves_like 'a type detector', 'var1'
+        # puppet compiler won't see $n, but we do
+        it_behaves_like 'a type inferrer', 'n', 'Numeric'
+        # We cant ever _really_ know the the result of .map so assume an Array[Any]
+        it_behaves_like 'a type inferrer', 'var2', 'Array'
+      end
+    end
+  end
+end
